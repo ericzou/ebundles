a63b76a91310ab17955de1d3634bc9f667f7388d
gcheshire <gcheshire@97847aa4-3a28-0410-b7a9-0d2d99b8f137>
2007-02-21 06:03:56 +0000

    git-svn-id: http://ebundles.googlecode.com/svn/trunk@46 97847aa4-3a28-0410-b7a9-0d2d99b8f137

--- Bundles/Blogging.tmbundle/Support/lib/blogging.rb
+++ Bundles/Blogging.tmbundle/Support/lib/blogging.rb
@@ -718,7 +718,9 @@ TEXT
               print "!#{url} (${1:#{alt}})!"
             else
               height_width = ""
-              height, width = %x{identify #{e_sh(full_path)}}.delete(e_sh(full_path)).scan(/\d+x\d+/)[0].split('x')
+              image_info_cmd = ENV['TM_BUNDLE_SUPPORT']} + "/bin/ImageInfo"
+              image_winfile = %x{cygpath -w "#{full_path}"}.chomp
+              height, width = %x{ #{image_info_cmd} "#{image_winfile}" }.split[2, 3]
               if height && width
                 height_width = %Q{ height="#{height}" width="#{width}"}
               end
