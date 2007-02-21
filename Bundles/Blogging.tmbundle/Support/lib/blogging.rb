1493081f1a4e1dea79985a053b1e1e4081c6d994
gcheshire <gcheshire@97847aa4-3a28-0410-b7a9-0d2d99b8f137>
2007-02-21 07:09:54 +0000

    Fix width, height order in upload_image's call to ImageInfo.
    git-svn-id: http://ebundles.googlecode.com/svn/trunk@50 97847aa4-3a28-0410-b7a9-0d2d99b8f137

--- Bundles/Blogging.tmbundle/Support/lib/blogging.rb
+++ Bundles/Blogging.tmbundle/Support/lib/blogging.rb
@@ -720,7 +720,7 @@ TEXT
               height_width = ""
               image_info_cmd = ENV['TM_BUNDLE_SUPPORT'] + "/bin/ImageInfo"
               image_winfile = %x{cygpath -w "#{full_path}"}.chomp
-              height, width = %x{ #{image_info_cmd} "#{image_winfile}" }.split[2, 3]
+              width, height = %x{"#{image_info_cmd}" "#{image_winfile}" }.split[2, 3]
               if height && width
                 height_width = %Q{ height="#{height}" width="#{width}"}
               end
